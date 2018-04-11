260df4cfb0073c8e8d67b5c82679fb0a3f920340
elminsterrr <przemyslaw.glab@gmail.com>
2018-04-11 14:44:12 +0200

    Add 'my-react-starter'

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+// import GuessEngine from '../GuessEngine';
+
+const App = () => (
+  <div className="App-container">
+    <p>The App</p>
+  </div>
+);
+
+export default App;
